b660a9c4539e0c44b056094d7d40dc356e59ef75
Leonard Huang <leihuang.org@gmail.com>
2017-04-16 18:43:59 +0800

    Update args.c

--- src/args.c
+++ src/args.c
@@ -138,7 +138,7 @@ static int parse_user_tokens(shadowvpn_args_t *args, char *value) {
   args->user_tokens = calloc(len, 8);
   bzero(args->user_tokens, 8 * len);
   value = start;
-	
+  /*
   char *temp;
   char *tokens_temp = value;
   while ((temp = strtok(tokens_temp, ",")) != NULL) {
@@ -147,7 +147,7 @@ static int parse_user_tokens(shadowvpn_args_t *args, char *value) {
      i++;
      tokens_temp = NULL;
   }
-  /*
+  */
   while (*value) {
     int has_next = 0;
     sp_pos = strchr(value, ',');
@@ -174,7 +174,6 @@ static int parse_user_tokens(shadowvpn_args_t *args, char *value) {
       break;
     }
   }
-  */
   free(start);
   return 0;
 }
